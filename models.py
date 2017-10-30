4ecad903a58d00bf60af5c6d51fc499cc8bd0600
timmo <1530731815@qq.com>
2017-10-30 16:28:33 +0800

    up

--- models.py
+++ models.py
@@ -18,7 +18,8 @@ class Article(db.Model):
 	content = db.Column(db.Text)
 	pub_time = db.Column(db.DateTime, default = datetime.now)
 	tags = db.relationship('Tag',
-		secondary =)
+		secondary = articles_tags,
+		backref = db.backref('articles', lazy = 'dynamic'))
 
 
 	def __init__(self, title, summary, content, pub_time = None):
@@ -33,7 +34,7 @@ class Tag(db.Model):
 	__tablename__ = 'tags'
 
 	id = db.Column(db.Integer, primary_key = True)
-	name = db.Column(db.String, unique = True)
+	name = db.Column(db.String(50), unique = True)
 
 	def __init__(self, name):
 		self.name = name.lower()
